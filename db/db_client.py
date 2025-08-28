e5e4e0cf5966697a9400de8287dfc1e199555bcf
Ernest Lin <linchongtik@gmail.com>
2025-08-27 17:06:52 -0700

    connect mongoDB, add the news to the database

---
+++ db/db_client.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from pymongo import MongoClient
+import certifi
+
+#Connent database
+url =os.getenv("DB_URL")
+client = MongoClient(url, tlsCAFile=certifi.where())
+
+#create database and collection
+db=client["ai_news_agent"]
+news_collection = db["news"]
+
--- services/news_fetcher.py
+++ services/news_fetcher.py
@@ -1,13 +1,26 @@
 from langchain_tavily import TavilySearch
+from db.db_client import news_collection 
+
+def save_news_to_db(news_item):
+    if news_collection.count_documents({"url":news_item["url"]},limit=1) == 0: 
+        news_collection.insert_one(news_item)
+    else:
+        print("news already exist in DB")
 
 def fetch_news(topic:str):
     tool = TavilySearch(max_results=5, topic="news", time_range="day",)
     results = tool.invoke({"query":topic})
     search_result=results["results"]
 
-    context=[
-        {"title":doc["title"],"url":doc['url'], "published_date":doc['published_date'], "content":doc['content']}
-         for doc in search_result
-    ]
+    context=[]
+    for doc in search_result:
+       news_item = {
+          "title":doc["title"],
+          "url":doc['url'], 
+          "published_date":doc['published_date'], 
+          "content":doc['content']
+       }
+       save_news_to_db(news_item)
+       context.append(news_item)
 
     return context
